5832fba9974e5529c6f8a16e1fc3fbaba557fb89
Alshaikhly <alshaikhly89@gmail.com>
2020-07-21 17:30:32 +0000

    One-to-many

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Ali", last_name: "Moe", hourly_rate: 70)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 50)
+@store1.employees.create(first_name: "Joe", last_name: "Doe", hourly_rate: 15)
